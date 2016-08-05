73bdd27c08c6906cce87f18d6546544aaf375224
kristofsajdak <kristof.sajdak@gmail.com>
2016-08-05 23:45:56 +0200

    implemented happy path of sse observable to http streaming + test

---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const Stream = require('stream');
+
+const endl = '\r\n';
+
+module.exports.handle = (sseObservable, req, reply) => {
+
+    const stream = new Stream.PassThrough();
+    stream.write('');
+
+    reply(stream)
+        .type('text/event-stream')
+        .header('Cache-Control', 'no-cache')
+        .header('Connection', 'keep-alive')
+        .header('Content-Encoding', 'identity');
+
+    var subscription = sseObservable
+        .subscribe(
+            (sseObject) => {
+                const stringifiedSSEPart = stringifyEvent(sseObject);
+                stream.write(stringifiedSSEPart)
+            },
+            (err) => {
+                console.error(err.stack);
+                end()
+            });
+
+    // this is triggered on when the client issues a req.abort() or when the connection closes
+    req.raw.req.on('close', function () {
+        end()
+    });
+
+    function end() {
+        subscription.dispose();
+        stream.end();
+    }
+
+    function stringifyEvent(sseObject) {
+        let str = '';
+        if (sseObject.event) {
+            str += 'event: ' + sseObject.event + endl
+        } else {
+            str += 'event: message';
+        }
+        sseObject.data && (str += 'data: ' + sseObject.data + endl);
+        sseObject.id && (str += 'id: ' + sseObject.id + endl);
+        str += endl;
+        return str;
+    }
+
+};
---
+++ test/sse.spec.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Rx = require('rx');
+const uuid = require('node-uuid');
+const EventSource = require('eventsource');
+const chai = require('chai');
+const expect = chai.expect;
+const hapiRxSSE = require('../');
+
+const baseUrl = 'http://localhost:9100';
+
+describe('Given a Hapi server ' +
+    'And a route handler which uses hapi-rx-sse to broadcast an observable ' +
+    'which emits elements from a predefined array', function () {
+
+    beforeEach(function () {
+
+        const sseArray = [
+            {
+                id: '1',
+                event: 'books.insert',
+                data: JSON.stringify({
+                    id: 5,
+                    attributes: {
+                        title: 'test title5'
+                    }
+                })
+            },
+            {
+                id: '2',
+                event: 'books.insert',
+                data: JSON.stringify({
+                    id: 6,
+                    attributes: {
+                        title: 'test title6'
+                    }
+                })
+            }
+        ];
+
+        const server = this.server = new Hapi.Server();
+        server.connection({port: 9100});
+
+        return server.start()
+            .then(() => {
+                server.route({
+                    path: '/events/streaming',
+                    method: 'GET',
+                    handler: (req, reply) => {
+                        const sseObservable = Rx.Observable.fromArray(sseArray);
+                        hapiRxSSE.handle(sseObservable, req, reply);
+                    }
+                });
+
+                return server
+            });
+
+    });
+
+    afterEach(function () {
+        return this.server.stop();
+    });
+
+    describe('When an EventSource is created', function () {
+
+        beforeEach(function () {
+            this.source = new EventSource(baseUrl + '/events/streaming');
+        });
+
+        it('Then it should receive the events contained in the predefined array', function (done) {
+
+            Rx.Observable.fromEvent(this.source, 'books.insert')
+                .bufferWithCount(2)
+                .subscribe(
+                    (events) => {
+                        assertEventState(events[0], '1', 'books.insert', 5, 'test title5');
+                        assertEventState(events[1], '2', 'books.insert', 6, 'test title6');
+                        this.source.close();
+                        done();
+                    },
+                    done)
+
+        })
+    });
+});
+
+function assertEventState(event, id, type, resId, resAttId) {
+    expect(event.lastEventId).to.equal(id);
+    expect(event.type).to.equal(type);
+    var parsed = JSON.parse(event.data);
+    expect(parsed).to.deep.equal({
+        id: resId,
+        attributes: {
+            title: resAttId
+        }
+    })
+}
+
